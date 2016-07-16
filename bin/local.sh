dd4439d6b813125b5d7a7667c476c5ea2a6d098c
Alan Colon <github@alanc.net>
2016-07-16 08:59:48 -0600

    Added transmission

--- bin/local.sh
+++ bin/local.sh
@@ -4,7 +4,9 @@ gateway=$(tokens=($(route | grep -e '^default')); echo ${tokens[1]})
 echo "vpn: $vpn; gateway: $gateway"
 route del default
 route add $vpn gw $gateway
+route add -net 192.168.0.0/16 gw $gateway dev eth0
 
 export VPN_HOST=$vpn; 
 cat client.openvpn | envsubst > runtime.openvpn
-openvpn --config runtime.openvpn
+openvpn --config runtime.openvpn &
+transmission-daemon --foreground --config-dir /etc/transmission-daemon
--- local.Dockerfile
+++ local.Dockerfile
@@ -1,6 +1,16 @@
 FROM torrent-vpn-base
+
+RUN apt-get install -y transmission-daemon
+
 COPY client.openvpn /
+
+COPY /etc/transmission-daemon/* /etc/transmission-daemon/
+
+EXPOSE 39091
+
+VOLUME /var/lib/transmission-daemon
 
 CMD local.sh
 
+USER root
 
